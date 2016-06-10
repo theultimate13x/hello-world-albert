f0d5809fffa051575246296adf26c53189b6b5e4
Lucky 13 X <theultimate13x@hotmail.com>
2016-06-10 15:02:51 -0500

    Starting out with my Tic Tac Toe pseudo-code

---
+++ tic_tac_toe/script.js
@@ -0,0 +1,15 @@
+//Try to make code as DRY as possible, or complex because you are still lost in class
+
+
+//Make an array to show values 0-8. Consider using buttons or just having JS check for whether there is an X or O value, and then saving that information.
+
+//Compare if there is an O or X present
+
+
+//Checking to see if there is a 3 in a row on a row
+
+//Checking to see if there is a 3 in a row on a column
+
+//If understood how, try using elements or objects to accomplish goal
+
+//Consider using a JS library that makes grid placement easier.
